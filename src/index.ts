419072415aa25ad56e572c5c788cb2138f73f4fa
newraina <newraina@users.noreply.github.com>
2019-01-11 17:41:38 +0800

    add support for mst array

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export type ListItemType<T extends any[]> = T extends Array<infer R> ? R : any
+import { IMSTArray } from 'mobx-state-tree'
+
+export type ListItemType<T extends any[]> = T extends IMSTArray<infer R>
+  ? R
+  : T extends Array<infer R>
+  ? R
+  : any
